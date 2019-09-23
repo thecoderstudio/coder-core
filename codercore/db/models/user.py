50c44c7d68669b47ae0b8c630017f6d004d47233
Robin Siep <RobinSiep1@gmail.com>
2019-09-23 10:27:16 +0200

    unmixin

--- codercore/db/models/user.py
+++ codercore/db/models/user.py
@@ -7,7 +7,7 @@ from codercore.db import Base, DBSession as session
 from codercore.db.type import UUID
 
 
-class UserMixin(Base):
+class User(Base):
     __tablename__ = 'user'
 
     id = Column(UUID, primary_key=True, default=uuid.uuid4)
@@ -19,4 +19,4 @@ class UserMixin(Base):
 
 
 def get_user_by_email(email):
-    return session.query(UserMixin).filter(UserMixin.email == email)
+    return session.query(User).filter(User.email == email)
