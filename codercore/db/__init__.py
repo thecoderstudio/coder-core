6f9bdb91a3c032d5514f26520de700a7bb6e85aa
Robin Siep <RobinSiep1@gmail.com>
2019-10-29 11:12:06 +0100

    use register instead

--- codercore/db/__init__.py
+++ codercore/db/__init__.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-from zope.sqlalchemy import ZopeTransactionEvents
+from zope.sqlalchemy import register
 
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionEvents()))
+DBSession = scoped_session(sessionmaker())
+register(DBSession)
 Base = declarative_base()
