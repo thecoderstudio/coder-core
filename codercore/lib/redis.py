b2d31d6c5089b80dbe3df547ea2e98689b855f09
Robin Siep <RobinSiep1@gmail.com>
2022-10-26 16:34:06 +0200

    redis tests

--- codercore/lib/redis.py
+++ codercore/lib/redis.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
+
 from functools import cache
-from typing import Optional
+from typing import Any, Awaitable, Optional, Union
 
 from redis import StrictRedis
 
@@ -9,6 +11,36 @@ def connection(
     host: str,
     port: int = 6379,
     db: int = 0,
-    password: Optional[str] = None
-) -> StrictRedis:
-    return StrictRedis(host=host, port=port, db=db, password=password)
+    password: Optional[str] = None,
+    default_ttl_in_seconds: Optional[int] = None
+) -> Redis:
+    return Redis(
+        host=host,
+        port=port,
+        db=db,
+        password=password,
+        default_ttl_in_seconds=default_ttl_in_seconds
+    )
+
+
+class Redis(StrictRedis):
+    default_ttl_in_seconds: Optional[int]
+
+    def __init__(
+        self,
+        *args,
+        default_ttl_in_seconds: Optional[int] = None,
+        **kwargs
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.default_ttl_in_seconds = default_ttl_in_seconds
+
+    def set(
+        self,
+        *args,
+        ex: Optional[int] = None,
+        **kwargs
+    ) -> Union[Awaitable, Any]:
+        if not ex:
+            ex = self.default_ttl_in_seconds
+        super().set(*args, ex=ex, **kwargs)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,17 @@
+from collections.abc import Iterator
+
 from pytest import fixture
+from sqlalchemy.orm import sessionmaker as sqlalchemy_sessionmaker, Session
 from sqlalchemy_utils import database_exists, create_database
 
 from codercore.db import sessionmaker
 from codercore.db.models import Base
+from codercore.lib.redis import connection, Redis
 from codercore.lib.settings import EnvSettings
 
 
 @fixture(scope='session')
-def db_sessionmaker():
+def db_sessionmaker() -> sqlalchemy_sessionmaker:
     connection_url = EnvSettings.database_connection_url
     if not database_exists(connection_url):
         create_database(connection_url)
@@ -15,10 +19,15 @@ def db_sessionmaker():
 
 
 @fixture
-def db_session(db_sessionmaker):
+def db_session(db_sessionmaker: sqlalchemy_sessionmaker) -> Iterator[Session]:
     try:
         Base.metadata.create_all()
         with db_sessionmaker() as session:
             yield session
     finally:
         Base.metadata.drop_all()
+
+
+@fixture
+def redis_connection() -> Redis:
+    return connection(**EnvSettings.redis)
