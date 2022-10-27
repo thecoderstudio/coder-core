dee35dc806b2859698c6d031549854513c6fefda
Robin Siep <RobinSiep1@gmail.com>
2022-10-27 12:12:13 +0200

    db tests

--- codercore/db/__init__.py
+++ codercore/db/__init__.py
@@ -1,7 +1,6 @@
 from functools import cache
 
 from sqlalchemy import create_engine
-from sqlalchemy.engine import Engine
 from sqlalchemy.orm import sessionmaker as sessionmaker_, Session as Session_
 
 from codercore.db.models import Base
@@ -17,11 +16,6 @@ def sessionmaker(connection_url: str, *args, **kwargs) -> sessionmaker_:
     engine = create_engine(connection_url)
     Base.metadata.bind = engine
     return sessionmaker_(engine, *args, **kwargs)
-
-
-def init_sqlalchemy(connection_url: str) -> Engine:
-    engine = create_engine(connection_url)
-    return engine
 
 
 def get_connection_url(driver: str, user: str, password: str, host: str,
---
+++ tests/db/test_db.py
@@ -0,0 +1,44 @@
+from unittest.mock import patch
+
+from codercore.db import get_connection_url, sessionmaker, Session
+
+
+def test_get_connection_url():
+    connection_url = get_connection_url(**{
+        'driver': 'postgresql',
+        'user': 'test',
+        'password': 'test',
+        'host': 'localhost',
+        'database': 'test'
+    })
+
+    assert connection_url == "postgresql://test:test@localhost/test"
+
+
+def test_sessionmaker(mocker):
+    connection_url = "postgresql://test:test@localhost/test"
+    engine = mocker.MagicMock()
+    with (
+        patch('codercore.db.create_engine',
+              return_value=engine) as engine_mock,
+        patch('codercore.db.Base') as base_mock,
+        patch('codercore.db.sessionmaker_') as sessionmaker_mock
+    ):
+        sessionmaker(connection_url)
+
+    engine_mock.assert_called_once_with(connection_url)
+    base_mock.metadata.bind == engine
+    sessionmaker_mock.assert_called_once_with(engine)
+
+
+def test_session(mocker):
+    session_mock = mocker.MagicMock()
+    connection_url = "postgresql://test:test@localhost/test"
+    with patch(
+        'codercore.db.sessionmaker',
+        return_value=session_mock
+    ) as sessionmaker_mock:
+        session = Session(connection_url)
+
+    sessionmaker_mock.assert_called_once_with(connection_url)
+    assert session == session_mock()
