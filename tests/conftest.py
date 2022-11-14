bc10d222a39f20ee02b3154c3bb4c1e788c51ff2
Robin Siep <RobinSiep1@gmail.com>
2022-11-14 12:51:26 +0100

    expose fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,49 +3,34 @@ from collections.abc import AsyncIterator
 
 from pytest import fixture
 from sqlalchemy.orm import sessionmaker as sqlalchemy_sessionmaker, Session
-from sqlalchemy_utils import database_exists, create_database
 
-from codercore.db import get_connection_url, sessionmaker
-from codercore.db.models import Base
-from codercore.lib.redis import connection, Redis
-from codercore.lib.settings import EnvSettings
+from codercore.lib.redis import Redis
+from codercore.test.fixtures import (
+    db_sessionmaker as db_sessionmaker_,
+    db_session as db_session_,
+    redis_connection as redis_connection_
+)
 
 
 @fixture
 def db_sessionmaker(worker_id: str) -> sqlalchemy_sessionmaker:
-    connection_settings = {
-        'user': os.environ['POSTGRES_USER'],
-        'password': os.environ['POSTGRES_PASSWORD'],
-        'host': os.environ['POSTGRES_HOST'],
-        'database': worker_id
-    }
-
-    sync_connection_url = get_connection_url('postgresql',
-                                             **connection_settings)
-    async_connection_url = get_connection_url('postgresql+asyncpg',
-                                              **connection_settings)
-    if not database_exists(sync_connection_url):
-        create_database(sync_connection_url)
-    return sessionmaker(async_connection_url)
+    return db_sessionmaker_(
+        os.environ['POSTGRES_USER'],
+        os.environ['POSTGRES_PASSWORD'],
+        os.environ['POSTGRES_HOST'],
+        worker_id
+    )
 
 
 @fixture
 async def db_session(
     db_sessionmaker: sqlalchemy_sessionmaker
 ) -> AsyncIterator[Session]:
-    async with (
-        db_sessionmaker() as session,
-        session.bind.begin() as conn
-    ):
-        try:
-            await conn.run_sync(Base.metadata.drop_all)
-            yield session
-        finally:
-            await conn.run_sync(Base.metadata.create_all)
+    async for session in db_session_(db_sessionmaker):
+        yield session
 
 
 @fixture
 async def redis_connection(worker_id: str) -> AsyncIterator[Redis]:
-    redis = connection(db=int(worker_id[2:]), **EnvSettings.redis)
-    yield redis
-    await redis.close()
+    async for connection in redis_connection_(worker_id):
+        yield connection
