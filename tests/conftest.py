596ac28accb0fac81981d3ba1543150ebe3fdee8
Robin Siep <RobinSiep1@gmail.com>
2022-11-14 12:58:36 +0100

    call fixture on original method rather than decorating wrapper

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,16 @@
 import os
-from collections.abc import AsyncIterator
 
 from pytest import fixture
-from sqlalchemy.orm import sessionmaker as sqlalchemy_sessionmaker, Session
+from sqlalchemy.orm import sessionmaker as sqlalchemy_sessionmaker
 
-from codercore.lib.redis import Redis
 from codercore.test.fixtures import (
     db_sessionmaker as db_sessionmaker_,
     db_session as db_session_,
     redis_connection as redis_connection_
 )
+
+db_session = fixture(db_session_)
+redis_connection = fixture(redis_connection_)
 
 
 @fixture
@@ -20,17 +21,3 @@ def db_sessionmaker(worker_id: str) -> sqlalchemy_sessionmaker:
         os.environ['POSTGRES_HOST'],
         worker_id
     )
-
-
-@fixture
-async def db_session(
-    db_sessionmaker: sqlalchemy_sessionmaker
-) -> AsyncIterator[Session]:
-    async for session in db_session_(db_sessionmaker):
-        yield session
-
-
-@fixture
-async def redis_connection(worker_id: str) -> AsyncIterator[Redis]:
-    async for connection in redis_connection_(worker_id):
-        yield connection
