025e3f014884d7deb5dc7e46f8b17d1e8889c690
Robin Siep <RobinSiep1@gmail.com>
2023-02-09 15:47:38 +0100

    Allow custom connection creator and implement auto_iam_creator

--- codercore/db/__init__.py
+++ codercore/db/__init__.py
@@ -1,8 +1,10 @@
 from functools import cache
-from typing import Type, Optional
+from typing import Callable, Optional, Type
 
+from asyncpg.connection import Connection
+from google.cloud.sql.connector import Connector, IPTypes
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker as sessionmaker_, Session as Session_
+from sqlalchemy.orm import Session as Session_, sessionmaker as sessionmaker_
 from sqlalchemy.pool import Pool
 
 from codercore.db.models import Base
@@ -17,10 +19,14 @@ def Session(connection_url: str) -> Session_:  # noqa
 def sessionmaker(
     connection_url: str,
     *args,
+    creator: Callable | None = None,
     poolclass: Optional[Type[Pool]] = None,
     **kwargs,
 ) -> sessionmaker_:
-    engine = create_async_engine(connection_url, poolclass=poolclass)
+    engine_kwargs = {}
+    if creator:
+        engine_kwargs["creator"] = creator
+    engine = create_async_engine(connection_url, poolclass=poolclass, **engine_kwargs)
     Base.metadata.bind = engine
     return sessionmaker_(engine, *args, class_=AsyncSession, **kwargs)
 
@@ -29,3 +35,23 @@ def get_connection_url(
     driver: str, user: str, password: str, host: str, database: str
 ) -> str:
     return f"{driver}://{user}:{password}@{host}/{database}"
+
+
+def get_connection_with_auto_iam_creator(
+    instance_connection_name: str,
+    user: str,
+    database: str,
+) -> Callable[[], Connection]:
+    connector = Connector()
+
+    def creator() -> Connection:
+        return connector.connect(
+            instance_connection_name,
+            "asyncpg",
+            user=user,
+            db=database,
+            enable_iam_auth=True,
+            ip_type=IPTypes.PUBLIC,
+        )
+
+    return creator
--- codercore/lib/settings.py
+++ codercore/lib/settings.py
@@ -6,14 +6,34 @@ from codercore.db import get_connection_url
 class EnvSettings:
     @classmethod
     @property
+    def database(cls) -> dict[str, str]:
+        base = {
+            "user": os.environ["POSTGRES_USER"],
+            "database": os.environ["POSTGRES_DB"],
+        }
+        if os.environ.get("POSTGRES_USE_SQL_CONNECTOR", False):
+            return {
+                "instance_connection_name": os.environ[
+                    "POSTGRES_INSTANCE_CONNECTION_NAME"
+                ],
+                **base,
+            }
+        else:
+            return {
+                "password": os.environ["POSTGRES_PASSWORD"],
+                "host": os.environ["POSTGRES_HOST"],
+                **base,
+            }
+
+    @classmethod
+    @property
     def database_connection_url(cls) -> str:
-        return get_connection_url(
-            "postgresql+asyncpg",
-            os.environ["POSTGRES_USER"],
-            os.environ["POSTGRES_PASSWORD"],
-            os.environ["POSTGRES_HOST"],
-            os.environ["POSTGRES_DB"],
-        )
+        return get_connection_url("postgresql+asyncpg", **cls.database)
+
+    @classmethod
+    @property
+    def database_instance_connection_name(cls) -> str:
+        return os.environ["POSTGRES_INSTANCE_CONNECTION_NAME"]
 
     @classmethod
     @property
