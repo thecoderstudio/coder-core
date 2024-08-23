562971da5e92896e041a8838470a9f766ba8ebb1
Robin Siep <RobinSiep1@gmail.com>
2024-08-23 11:19:15 +0200

    Set attrs exclusion feature

--- codercore/lib/__init__.py
+++ codercore/lib/__init__.py
@@ -1,6 +1,8 @@
 from typing import Any
 
 
-def setattrs(obj: Any, **kwargs) -> None:
+def setattrs(obj: Any, exclude_none: bool = False, **kwargs) -> None:
     for key, value in kwargs.items():
+        if exclude_none and value is None:
+            continue
         setattr(obj, key, value)
--- tests/lib/test_lib.py
+++ tests/lib/test_lib.py
@@ -13,3 +13,11 @@ def test_set_attrs():
     assert sample.a == 2
     assert sample.b == "b"
     assert not sample.c
+
+
+def test_set_attrs_exclude_none():
+    sample = Sample()
+    setattrs(sample, exclude_none=True, a=None, c=False)
+    assert sample.a == 1
+    assert sample.b == "b"
+    assert not sample.c
